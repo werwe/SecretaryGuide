16cd8a10ed36de5bd3682a01c9fffbde63d49de5
Hyun Jin, Lee <werwe.me@gmail.com>
2014-10-24 17:49:48 +0900

    add controller

--- app/src/main/java/kr/co/starmark/secretaryguide/CompareActivity.java
+++ app/src/main/java/kr/co/starmark/secretaryguide/CompareActivity.java
@@ -6,8 +6,17 @@ import android.app.FragmentTransaction;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import butterknife.ButterKnife;
@@ -15,7 +24,12 @@ import butterknife.InjectView;
 import butterknife.OnClick;
 
 
-public class CompareActivity extends Activity implements MediaPlayer.OnPreparedListener{
+public class CompareActivity extends Activity implements MediaPlayer.OnPreparedListener, SeekBar.OnSeekBarChangeListener {
+
+    public static final int PLAY = 1;
+    public static final int PAUSE = 2;
+    public static final int STOP = 3;
+    private static final String TAG = CompareActivity.class.getName();
 
     @InjectView(R.id.left)
     Button mLeft;
@@ -24,15 +38,29 @@ public class CompareActivity extends Activity implements MediaPlayer.OnPreparedL
     @InjectView(R.id.right)
     ImageButton mRight;
 
-    @InjectView(R.id.greeting1)
+    @InjectView(R.id.face_explain)
     TextView mGreeting1;
+
     @InjectView(R.id.leftVideo)
-    android.widget.VideoView mLeftVideo;
+    VideoView mLeftVideo;
     @InjectView(R.id.rightVideo)
     VideoView mRightVideo;
 
     @InjectView(R.id.btn_play)
     ImageView mPlay;
+    @InjectView(R.id.video_frame)
+    LinearLayout mVideoFrame;
+    @InjectView(R.id.seek_bar)
+    SeekBar mSeekBar;
+
+    @InjectView(R.id.play)
+    Button mToolbarPlay;
+    @InjectView(R.id.controller)
+    LinearLayout mController;
+    @InjectView(R.id.duration)
+    TextView mDuration;
+
+    private int mPlayStatement = STOP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +69,13 @@ public class CompareActivity extends Activity implements MediaPlayer.OnPreparedL
         setContentView(R.layout.activity_compare);
         setActionBar();
         ButterKnife.inject(this);
-
         GreetingVideo video = getIntent().getParcelableExtra("record");
 
         video.log();
 
         String title = getGreetingType(video.type) + " - " + getfaceSide(video.side);
         mTitle.setText("Secretary Guide");
+        mRight.setVisibility(View.GONE);
         mGreeting1.setText(title);
         mRight.setEnabled(true);
         mLeftVideo.setVideoURI(getVideoUri(video.type, video.side));
@@ -56,13 +84,22 @@ public class CompareActivity extends Activity implements MediaPlayer.OnPreparedL
         mRightVideo.setVideoPath(video.path);
         mRightVideo.requestFocus();
 
-        mLeftVideo.seekTo(100);
-        mRightVideo.seekTo(100);
+        mLeftVideo.seekTo(0);
+        mRightVideo.seekTo(0);
 
         mRightVideo.setOnCompletionListener(mV2Completion);
         mLeftVideo.setOnCompletionListener(mV1Completion);
         mRightVideo.setOnPreparedListener(this);
         mLeftVideo.setOnPreparedListener(this);
+
+        mSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mThread != null && mThread.isAlive())
+            mThread.interrupt();
     }
 
     private String getfaceSide(int side) {
@@ -88,7 +125,7 @@ public class CompareActivity extends Activity implements MediaPlayer.OnPreparedL
     }
 
     private Uri getVideoUri(int type, int side) {
-        return Uri.parse("android.resource://" + getPackageName() + "/raw/greeting_sample_"+type+"_"+side);
+        return Uri.parse("android.resource://" + getPackageName() + "/raw/greeting_sample_" + type + "_" + side);
     }
 
     private void setActionBar() {
@@ -96,7 +133,6 @@ public class CompareActivity extends Activity implements MediaPlayer.OnPreparedL
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         actionBar.setCustomView(R.layout.actionbar);
         ButterKnife.inject(actionBar.getCustomView());
-        //mTitle.setText("Secretary Guide");
     }
 
     @OnClick(R.id.left)
@@ -105,8 +141,7 @@ public class CompareActivity extends Activity implements MediaPlayer.OnPreparedL
     }
 
     @OnClick(R.id.right)
-    public void right()
-    {
+    public void right() {
         MenuDialogFragment dialog = MenuDialogFragment.newInstance(0);
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         dialog.show(ft, "dialog");
@@ -119,8 +154,10 @@ public class CompareActivity extends Activity implements MediaPlayer.OnPreparedL
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
             mV1IsPlaying = false;
-            if(!mV2IsPlaying)
+            if (!mV2IsPlaying) {
                 mPlay.setVisibility(View.VISIBLE);
+                mThread.interrupt();
+            }
         }
     };
 
@@ -128,22 +165,101 @@ public class CompareActivity extends Activity implements MediaPlayer.OnPreparedL
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
             mV2IsPlaying = false;
-            if(!mV1IsPlaying)
+            if (!mV1IsPlaying) {
                 mPlay.setVisibility(View.VISIBLE);
+                mThread.interrupt();
+            }
         }
     };
 
     @Override
     public void onPrepared(MediaPlayer mediaPlayer) {
-        mediaPlayer.seekTo(100);
+        mediaPlayer.seekTo(0);
+        int duration = mLeftVideo.getDuration();
+        mSeekBar.setMax(duration);
+
+//        Log.d(TAG, String.format("duration:%d,%d", duration, mSeekBar.getMax()));
     }
 
-    @OnClick(R.id.btn_play)
-    public void onPlay(View v) {
+    public void play() {
+        mPlayStatement = PLAY;
         mLeftVideo.start();
         mRightVideo.start();
         mV1IsPlaying = true;
         mV2IsPlaying = true;
-        v.setVisibility(View.INVISIBLE);
+        mPlay.setVisibility(View.INVISIBLE);
+        mToolbarPlay.setText("pause");
+        if (mThread != null && mThread.isAlive()) {
+            mThread.interrupt();
+        }
+        mThread = new SeekerThread();
+        mThread.start();
+
     }
+
+    SeekerThread mThread;
+
+    @OnClick(R.id.btn_play)
+    public void onPlay(View v) {
+        play();
+    }
+
+    @OnClick(R.id.play)
+    public void onPlayPause(View v) {
+        if (mPlayStatement == STOP || mPlayStatement == PAUSE) {
+            play();
+        } else if (mPlayStatement == PLAY) {
+            mPlayStatement = PAUSE;
+            mPlay.setVisibility(View.VISIBLE);
+            mToolbarPlay.setText("play");
+            mLeftVideo.pause();
+            mRightVideo.pause();
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        if (b) {
+            mLeftVideo.seekTo(i);
+            mRightVideo.seekTo(i);
+            Log.d(TAG, String.format("seek:%d", i));
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    class SeekerThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            while (true) {
+                try {
+                    Thread.sleep(50);
+                    mLeftVideo.getCurrentPosition();
+                    Message msg = Message.obtain();
+                    msg.arg1 = mLeftVideo.getCurrentPosition();
+                    mHandler.sendMessage(msg);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int currentPosition = msg.arg1;
+            mSeekBar.setProgress(currentPosition);
+            mDuration.setText(DateFormat.format("m:ss", currentPosition) + " / " + DateFormat.format("m:ss", mLeftVideo.getDuration()));
+        }
+    };
 }
